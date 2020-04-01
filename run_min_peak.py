a2ddd14f0506d7da97a100a46d0f16944a66bd4f
Tristan Hearn <tristan.a.hearn@nasa.gov>
2020-04-01 11:51:32 -0400

    working new min_sq

--- run_min_peak.py
+++ run_min_peak.py
@@ -2,12 +2,11 @@ import numpy as np
 import openmdao.api as om
 import dymos as dm
 import matplotlib.pyplot as plt
-from openmdao.api import Problem, Group, pyOptSparseDriver, DirectSolver
 
-from pandemic import Pandemic
+from infection import Infection
 
-pop_total = 1.0 * 1e6
-infected0 = int(0.0005 * pop_total) + 1
+pop_total = 1.0
+infected0 = 1 / 500.0
 ns = 35
 
 p = om.Problem(model=om.Group())
@@ -16,107 +15,89 @@ traj = dm.Trajectory()
 
 p.model.add_subsystem('traj', subsys=traj)
 
-phase = dm.Phase(ode_class=Pandemic,
+phase = dm.Phase(ode_class=Infection,
                  transcription=dm.GaussLobatto(num_segments=ns, 
                                                order=3))
-p.model.linear_solver = DirectSolver()
-#phase.set_time_options(fix_initial=True, duration_bounds=(200.0, 301.0), units='d', targets=['sigma_comp.t'])
-phase.set_time_options(fix_initial=True, fix_duration=True, units='d', targets=['sigma_comp.t'])
+p.model.linear_solver = om.DirectSolver()
+phase.set_time_options(fix_initial=True, duration_bounds=(100.0, 301.0), targets=['t'])
+#phase.set_time_options(fix_initial=True, fix_duration=True)
 
 
-ds = 1e-3
-phase.add_state('susceptible', fix_initial=True, units='pax', rate_source='sdot', targets=['susceptible'],
-                ref=pop_total/2.0, defect_scaler = ds)
-phase.add_state('dead', fix_initial=True, units='pax', rate_source='ddot', targets=['dead'],
-                ref=pop_total/2.0, defect_scaler = ds)
-phase.add_state('infected', fix_initial=True, units='pax', rate_source='idot', targets=['infected'],
-                ref=pop_total/2.0, defect_scaler = ds)
-phase.add_state('immune', fix_initial=True, units='pax', rate_source='rdot', targets=['immune'],
-                ref=pop_total/2.0, defect_scaler = ds)
+ds = 1e-1
+phase.add_state('S', fix_initial=True, units='pax', rate_source='Sdot', targets=['S'], lower=0.0,
+                upper=pop_total, ref=pop_total/2, defect_scaler = ds)
+phase.add_state('I', fix_initial=True, units='pax', rate_source='Idot', targets=['I'], lower=0.0,
+                upper=pop_total, ref=pop_total/2, defect_scaler = ds)
+phase.add_state('R', fix_initial=True, units='pax', rate_source='Rdot', targets=['R'], lower=0.0,
+                upper=pop_total, ref=pop_total/2, defect_scaler = ds)
 
 #p.driver = om.ScipyOptimizeDriver()
 
-p.driver = pyOptSparseDriver()
+p.driver = om.pyOptSparseDriver()
 p.driver.options['optimizer'] = 'SNOPT'
 #p.driver.opt_settings['Major feasibility tolerance'] = 1.0E-8
-p.driver.opt_settings['Major optimality tolerance'] = 1.0
-p.driver.opt_settings["Major step limit"] = 0.5 #2.0
-p.driver.opt_settings['Major iterations limit'] = 1000000
-p.driver.opt_settings['Minor iterations limit'] = 1000000
-p.driver.opt_settings['Iterations limit'] = 1000000
+#p.driver.opt_settings['Major optimality tolerance'] = 1.0E-5
 p.driver.opt_settings['iSumm'] = 6
 
 p.driver.declare_coloring()
 
 
-t_on = 15.0 
-t_off = 55.0
 
-phase.add_input_parameter('t_on', units='d', targets=['sigma_comp.t_on'], dynamic=False, val=t_on)
-phase.add_input_parameter('t_off', units='d', targets=['sigma_comp.t_off'], dynamic=False, val=t_off)
+phase.add_input_parameter('beta', targets=['beta'], dynamic=True, val=0.25)
+phase.add_input_parameter('gamma', targets=['gamma'], dynamic=True, val=1.0/14.0)
+phase.add_input_parameter('t_on', targets=['t_on'], dynamic=False, val=10.0)
+phase.add_input_parameter('t_off', targets=['t_off'], dynamic=False, val=500.0)
+
+#phase.add_input_parameter('sigma', targets=['sigma'], dynamic=True, val=0.1)
 
 
-lim = 0.95
 
-phase.add_polynomial_control('sigma', targets=['sigma_comp.signal'], lower=0.05, upper=0.4, order=3)
+lim = 0.15
+phase.add_path_constraint('I', upper=lim)
 
-phase.add_objective('max_infected', ref=1e2)
+phase.add_control('sigma', targets=['sigma'], lower=0.0, upper=0.2, ref=0.1, fix_initial=True, fix_final=True)
 
-phase.add_timeseries_output('sigma_comp.filtered')
+phase.add_boundary_constraint('I', loc='final', upper=0.01)
+phase.add_objective('time', loc='final')
 
+phase.add_timeseries_output('theta')
 
 
 traj.add_phase(name='phase0', phase=phase)
 p.setup(check=True)
 
 p.set_val('traj.phase0.t_initial', 0)
-p.set_val('traj.phase0.t_duration', 90)
+p.set_val('traj.phase0.t_duration', 100)
 
-p.set_val('traj.phase0.states:susceptible',
+p.set_val('traj.phase0.states:S',
           phase.interpolate(ys=[pop_total - infected0, 0], nodes='state_input'))
-p.set_val('traj.phase0.states:infected',
-          phase.interpolate(ys=[infected0, 0], nodes='state_input'))
-p.set_val('traj.phase0.states:immune',
+p.set_val('traj.phase0.states:I',
+          phase.interpolate(ys=[infected0, pop_total/2], nodes='state_input'))
+p.set_val('traj.phase0.states:R',
           phase.interpolate(ys=[0, pop_total/2], nodes='state_input'))
-p.set_val('traj.phase0.states:dead',
-          phase.interpolate(ys=[1, 0], nodes='state_input'))
+
 
 p.run_driver()
 sim_out = traj.simulate()
 
 t = sim_out.get_val('traj.phase0.timeseries.time')
-s = sim_out.get_val('traj.phase0.timeseries.states:susceptible')
-i = sim_out.get_val('traj.phase0.timeseries.states:infected')
-r = sim_out.get_val('traj.phase0.timeseries.states:immune')
-d = sim_out.get_val('traj.phase0.timeseries.states:dead')
+s = sim_out.get_val('traj.phase0.timeseries.states:S')
+i = sim_out.get_val('traj.phase0.timeseries.states:I')
+r = sim_out.get_val('traj.phase0.timeseries.states:R')
 
-
-
-theta = sim_out.get_val('traj.phase0.timeseries.filtered')
-
-try:
-  sigma = sim_out.get_val('traj.phase0.timeseries.controls:sigma')
-except:
-  sigma = sim_out.get_val('traj.phase0.timeseries.polynomial_controls:sigma')
+theta = sim_out.get_val('traj.phase0.timeseries.theta')
 
 
 fig = plt.figure(figsize=(10, 5))
 plt.subplot(211)
-plt.title('polynomial mitigation between days %2.2f and %2.2f' % (t_on, t_off))
-plt.plot([t_on,t_on], [0, 1], 'k--', linewidth=0.9)
-plt.plot([t_off,t_off], [0, 1], 'k--', linewidth=0.9)
+plt.title('mitigation starting t = 10.0')
 plt.plot(t, i/pop_total, label='infected')
 plt.plot(t, s/pop_total, label='susceptible')
 plt.plot(t, r/pop_total, label='recovd/immune')
-plt.plot(t, d/pop_total, label='dead')
 plt.xlabel('days')
 plt.ylabel('pct. pop')
 plt.legend(loc=1)
-
 plt.subplot(212)
-plt.plot([t_on,t_on], [0, 1], 'k--', linewidth=0.9)
-plt.plot([t_off,t_off], [0, 1], 'k--', linewidth=0.9)
-plt.plot(t, theta, label='eff. contact rate $\\theta$')
-plt.plot(t, sigma, label='control $\\sigma$')
-plt.legend()
+plt.plot(t, theta)
+
 plt.show()
--- run_min_sigma_sq.py
+++ run_min_sigma_sq.py
@@ -2,12 +2,11 @@ import numpy as np
 import openmdao.api as om
 import dymos as dm
 import matplotlib.pyplot as plt
-from openmdao.api import Problem, Group, pyOptSparseDriver, DirectSolver
 
-from pandemic import Pandemic
+from infection import Infection
 
-pop_total = 1.0 * 1e6
-infected0 = int(0.0005 * pop_total) + 1
+pop_total = 1.0
+infected0 = 1 / 500.0
 ns = 35
 
 p = om.Problem(model=om.Group())
@@ -16,28 +15,26 @@ traj = dm.Trajectory()
 
 p.model.add_subsystem('traj', subsys=traj)
 
-phase = dm.Phase(ode_class=Pandemic,
+phase = dm.Phase(ode_class=Infection,
                  transcription=dm.GaussLobatto(num_segments=ns, 
                                                order=3))
-p.model.linear_solver = DirectSolver()
-phase.set_time_options(fix_initial=True, duration_bounds=(70.0, 301.0), units='d', targets=['sigma_comp.t'])
-#phase.set_time_options(fix_initial=True, fix_duration=True, units='d')
+p.model.linear_solver = om.DirectSolver()
+phase.set_time_options(fix_initial=True, duration_bounds=(100.0, 301.0), targets=['t'])
+#phase.set_time_options(fix_initial=True, fix_duration=True)
 
 
-ds = 1e-4
-phase.add_state('susceptible', fix_initial=True, units='pax', rate_source='sdot', targets=['susceptible'], lower=0.0,
+ds = 1e-1
+phase.add_state('S', fix_initial=True, units='pax', rate_source='Sdot', targets=['S'], lower=0.0,
                 upper=pop_total, ref=pop_total/2, defect_scaler = ds)
-phase.add_state('dead', fix_initial=True, units='pax', rate_source='ddot', targets=['dead'], lower=0.0,
+phase.add_state('I', fix_initial=True, units='pax', rate_source='Idot', targets=['I'], lower=0.0,
                 upper=pop_total, ref=pop_total/2, defect_scaler = ds)
-phase.add_state('infected', fix_initial=True, units='pax', rate_source='idot', targets=['infected'], lower=0.0,
+phase.add_state('R', fix_initial=True, units='pax', rate_source='Rdot', targets=['R'], lower=0.0,
                 upper=pop_total, ref=pop_total/2, defect_scaler = ds)
-phase.add_state('immune', fix_initial=True, units='pax', rate_source='rdot', targets=['immune'], lower=0.0,
-                upper=pop_total, ref=pop_total/2, defect_scaler = ds)
-phase.add_state('sum_sigma_sq', rate_source='sigma_comp.filtered_timescaled', defect_scaler = ds, fix_initial=True )
+phase.add_state('int_sigma', rate_source='sigma_sq', lower=0.0)
 
 #p.driver = om.ScipyOptimizeDriver()
 
-p.driver = pyOptSparseDriver()
+p.driver = om.pyOptSparseDriver()
 p.driver.options['optimizer'] = 'SNOPT'
 #p.driver.opt_settings['Major feasibility tolerance'] = 1.0E-8
 #p.driver.opt_settings['Major optimality tolerance'] = 1.0E-5
@@ -46,75 +43,65 @@ p.driver.opt_settings['iSumm'] = 6
 p.driver.declare_coloring()
 
 
-t_on = 10.0 
-t_off = 500.0 # always on afterwards
 
-phase.add_input_parameter('t_on', units='d', targets=['sigma_comp.t_on'], dynamic=False, val=t_on)
-phase.add_input_parameter('t_off', units='d', targets=['sigma_comp.t_off'], dynamic=False, val=t_off)
+phase.add_input_parameter('beta', targets=['beta'], dynamic=True, val=0.25)
+phase.add_input_parameter('gamma', targets=['gamma'], dynamic=True, val=1.0/14.0)
+phase.add_input_parameter('t_on', targets=['t_on'], dynamic=False, val=10.0)
+phase.add_input_parameter('t_off', targets=['t_off'], dynamic=False, val=500.0)
+
+#phase.add_input_parameter('sigma', targets=['sigma'], dynamic=True, val=0.1)
+
 
 
 lim = 0.15
-phase.add_path_constraint('infected', units='pax', upper=lim * pop_total, ref=lim*pop_total)
+phase.add_path_constraint('I', upper=lim)
 
-phase.add_control('sigma', targets=['sigma_comp.signal'], lower=0.1, upper=0.4, ref=0.4)
+phase.add_control('sigma', targets=['sigma'], lower=0.0, upper=0.2, ref=0.1, fix_initial=True, fix_final=True)
 
-phase.add_boundary_constraint('infected', loc='final', upper=2*infected0, ref=infected0)
-phase.add_objective('sum_sigma_sq', loc='final', ref=20.0)
+phase.add_boundary_constraint('I', loc='final', upper=0.01)
+phase.add_objective('int_sigma', loc='final')
+
+phase.add_timeseries_output('theta')
 
 
 traj.add_phase(name='phase0', phase=phase)
 p.setup(check=True)
 
 p.set_val('traj.phase0.t_initial', 0)
-p.set_val('traj.phase0.t_duration', 300)
+p.set_val('traj.phase0.t_duration', 100)
 
-p.set_val('traj.phase0.states:susceptible',
+p.set_val('traj.phase0.states:S',
           phase.interpolate(ys=[pop_total - infected0, 0], nodes='state_input'))
-p.set_val('traj.phase0.states:infected',
-          phase.interpolate(ys=[infected0, 0], nodes='state_input'))
-p.set_val('traj.phase0.states:immune',
+p.set_val('traj.phase0.states:I',
+          phase.interpolate(ys=[infected0, pop_total/2], nodes='state_input'))
+p.set_val('traj.phase0.states:R',
           phase.interpolate(ys=[0, pop_total/2], nodes='state_input'))
-p.set_val('traj.phase0.states:dead',
-          phase.interpolate(ys=[1, 0], nodes='state_input'))
-p.set_val('traj.phase0.states:sum_sigma_sq',
-          phase.interpolate(ys=[0.6, 0.6], nodes='state_input'))
+
 
 p.run_driver()
 sim_out = traj.simulate()
 
 t = sim_out.get_val('traj.phase0.timeseries.time')
-s = sim_out.get_val('traj.phase0.timeseries.states:susceptible')
-i = sim_out.get_val('traj.phase0.timeseries.states:infected')
-r = sim_out.get_val('traj.phase0.timeseries.states:immune')
-d = sim_out.get_val('traj.phase0.timeseries.states:dead')
+s = sim_out.get_val('traj.phase0.timeseries.states:S')
+i = sim_out.get_val('traj.phase0.timeseries.states:I')
+r = sim_out.get_val('traj.phase0.timeseries.states:R')
 
-bs = sim_out.get_val('traj.phase0.timeseries.states:sum_sigma_sq')
+int_sigma = sim_out.get_val('traj.phase0.timeseries.states:int_sigma')
+print("objective:", int_sigma[-1])
 
-
-try:
-    sigma = sim_out.get_val('traj.phase0.timeseries.controls:sigma')
-except:
-    try:
-        sigma = sim_out.get_val('traj.phase0.timeseries.polynomial_controls:sigma')
-    except:
-        sigma = np.ones(t.shape) * 0.4
+theta = sim_out.get_val('traj.phase0.timeseries.theta')
 
 
 fig = plt.figure(figsize=(10, 5))
 plt.subplot(211)
 plt.title('mitigation starting t = 10.0')
-plt.plot(t, len(t) * [lim], 'k:', linewidth=0.9, label='goal')
-plt.plot([t_on,t_on], [0, 1], 'k--', linewidth=0.9)
 plt.plot(t, i/pop_total, label='infected')
 plt.plot(t, s/pop_total, label='susceptible')
 plt.plot(t, r/pop_total, label='recovd/immune')
-plt.plot(t, d/pop_total, label='dead')
 plt.xlabel('days')
 plt.ylabel('pct. pop')
 plt.legend(loc=1)
-
 plt.subplot(212)
-plt.plot([t_on,t_on], [0, 1], 'k--', linewidth=0.9)
-plt.plot(t, sigma, label='$\\sigma$')
-plt.legend()
+plt.plot(t, theta)
+
 plt.show()
